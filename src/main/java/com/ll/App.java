df9776d389b43676c6132aad9543ce70b13f746b
jny0 <pjy100402@naver.com>
2023-03-06 15:34:21 +0900

    목록

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -11,7 +13,8 @@ public class App {
 
     public void run() {
         System.out.println("== 명언 앱 ==");
-        long lastWisSayingId = 1;
+        long lastWisSayingId = 0;
+        List<WiseSaying> wiseSayings = new ArrayList<>();
 
         while (true) {
             System.out.print("명령) ");
@@ -30,9 +33,19 @@ public class App {
                     System.out.print("작가 : ");
                     String authorName = sc.nextLine().trim();
 
-                    long id = lastWisSayingId++;
+                    long id = ++lastWisSayingId;
+                    wiseSayings.add(new WiseSaying(id, content, authorName));
 
                     System.out.printf("%d번 명언이 등록되었습니다.\n",id);
+                    break;
+                case "목록":
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("-".repeat(30));
+
+                    for(int i = wiseSayings.size() -1 ; i>=0; i--){
+                        WiseSaying wiseSaying = wiseSayings.get(i);
+                        System.out.printf("%d / %s / %s\n",wiseSaying.getId(), wiseSaying.getAuthorName(),wiseSaying.getContent());
+                    }
                     break;
                 default:
                     System.out.printf("`%s`(은)는 올바르지 않은 명령입니다.\n", cmd);
